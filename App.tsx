2555cce836555a844503b444fa0b2f19c507acad
Amadoubenthe <amadbenthebagnandiallo@gmail.com>
2023-06-30 16:49:15 +0000

    first commit

---
+++ App.tsx
@@ -0,0 +1,16 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+
+import {Text} from 'react-native';
+
+function App(): JSX.Element {
+  return <Text>DDDDBBDDDD</Text>;
+}
+
+export default App;
